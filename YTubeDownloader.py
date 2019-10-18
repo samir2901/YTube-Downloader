5a86a26562d50168fa885030696dd0a6539c79e3
samir2901 <thunder.cse2018@gmail.com>
2019-10-18 21:33:51 +0530

    Update
    Progress bar added

--- YTubeDownloader.py
+++ YTubeDownloader.py
@@ -1,16 +1,28 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from pytube import YouTube
+import time
+
+class Thread(QtCore.QThread):
+    change_value = QtCore.pyqtSignal(int)   
+    def run(self):
+        cnt = 0
+        while cnt <= 100:
+            cnt += 1
+            time.sleep(0.1)
+            self.change_value.emit(cnt)            
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(422, 230)
-        MainWindow.setMaximumSize(QtCore.QSize(422, 230))
+        MainWindow.resize(422, 270)
+        MainWindow.setMaximumSize(QtCore.QSize(422, 270))
         icon = QtGui.QIcon.fromTheme(":/image/YouTube-icon.png")
         MainWindow.setWindowIcon(icon)
-
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
+
+
         self.urlLabel = QtWidgets.QLabel(self.centralwidget)
         self.urlLabel.setGeometry(QtCore.QRect(20, 40, 111, 21))
         font = QtGui.QFont()
@@ -18,6 +30,7 @@ class Ui_MainWindow(object):
         font.setPointSize(12)
         self.urlLabel.setFont(font)
         self.urlLabel.setObjectName("urlLabel")
+
 
 
         self.url = QtWidgets.QLineEdit(self.centralwidget)
@@ -29,10 +42,25 @@ class Ui_MainWindow(object):
         self.url.setObjectName("url")
 
 
+
         self.download = QtWidgets.QPushButton(self.centralwidget)
         self.download.setGeometry(QtCore.QRect(160, 140, 101, 41))
         self.download.setObjectName("download")
         self.download.clicked.connect(self.downloadVideo)
+
+
+
+        self.progressBar = QtWidgets.QProgressBar(self.centralwidget)
+        self.progressBar.setGeometry(QtCore.QRect(30, 200, 371, 23))
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setAlignment(QtCore.Qt.AlignCenter)
+        self.progressBar.setTextVisible(True)
+        self.progressBar.setObjectName("progressBar")
+        self.progressBar.setMaximum(100)
+        self.progressBar.setMinimum(0)
+        self.progressBar.setValue(0)
+
+
 
 
         MainWindow.setCentralWidget(self.centralwidget)
@@ -53,28 +81,36 @@ class Ui_MainWindow(object):
         self.urlLabel.setText(_translate("MainWindow", "Enter the URL"))
         self.download.setText(_translate("MainWindow", "Download"))
 
-    def downloadVideo(self):
-        try:      
-            path = QtWidgets.QFileDialog.getExistingDirectory()
-            print(path)                                
-            link = self.url.text()
-            yt = YouTube(link)
-            video = yt.streams.first()
-            video.download(path)
+
+    def setProgressVal(self, val):
+        self.progressBar.setValue(val)  
+        if (self.progressBar.value()) == 100:             
+            self.progressBar.reset() 
             msgDone = QtWidgets.QMessageBox()
             msgDone.setIcon(QtWidgets.QMessageBox.Information)
             msgDone.setWindowTitle("Done")
             msgDone.setText("Download Complete")
-            msgDone.exec_()           
-            print("Download Done....")            
+            msgDone.exec_()     
+                
+
+    def downloadVideo(self):
+        try:      
+            path = QtWidgets.QFileDialog.getExistingDirectory()
+            print(path)                                
+            link = self.url.text()            
+            yt = YouTube(link)
+            video = yt.streams.first()            
+            video.download(path) 
+            self.thread = Thread()
+            self.thread.change_value.connect(self.setProgressVal)
+            self.thread.start() 
         except:
             msgError = QtWidgets.QMessageBox()
             msgError.setIcon(QtWidgets.QMessageBox.Critical)
             msgError.setWindowTitle("Error")
             msgError.setText("Error!!")
             msgError.exec_()
-            print("Connection Error.......")        
-
+            print("Connection Error.......") 
 
     
 import logo_rc
